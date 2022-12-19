f0e6afb8b00d89f717e09f64667d0415b6ff32e8
Jonathan Bautista <jon.bau.aco@outlook.com>
2022-12-19 14:10:44 -0700

    Se probaron las validaciones correctamente

--- js/validation.js
+++ js/validation.js
@@ -1,8 +1,12 @@
-let inputNombre = document.getElementById('nombre');
-let inputMail = document.getElementById('email');
+/*let inputNombre = document.getElementById('nombre');
+let inputMail = document.getElementById('correo');
 let inputTel = document.getElementById('telefono');
 let inputMensaje = document.getElementById('especificaciones');
-let btnEnviar = document.getElementById('enviar');
+let alertError = document.getElementById("alertError");
+*/
+let btnenviar = document.getElementById("btnEnviar");
+let idTimeout;
+
 
 // function validarNombre() {
 //     let regex = /^[A-Za-z]+$/;
@@ -12,7 +16,7 @@ let btnEnviar = document.getElementById('enviar');
 //     }
 // } //validarNombre
 
-function validarNombre(name) {
+/*function validarNombre(name) {
   let regex = /^[a-zA-Z\s]*$/;
   if (name.match(regex)) {
     console.log('valid name');
@@ -21,29 +25,11 @@ function validarNombre(name) {
     console.log('not a valid name');
   }
 } //validarNombre
+*/
 
 
-let alertError = document.getElementById("alertError");
-function fileValidation() {
-    let fileInput =
-        document.getElementById('file');
-     
-    let filePath = fileInput.value;
- 
-    // Allowing file type
-    let allowedExtensions =
-/(\.jpg|\.png|)$/i;
-     
-    if (!allowedExtensions.exec(filePath)) {
-        
-        alert('Archivo invalido por favor sube un archivo valido .png, .pdf, .jpg');
-        fileInput.value = '';
-        return false;
-    }
-};// validacion imagen
 
-
-function ValidationForm() {
+/*function ValidationForm() {
     let username = document.forms["RegForm"]["Name"];
     let email = document.forms["RegForm"]["Email"];
     let phoneNumber = document.forms["RegForm"]["Telephone"];
@@ -86,61 +72,85 @@ function ValidationForm() {
     }
     return true;
   };// validaciones internet
-
+*/
   //Ejercicios Strings
-let btnenviar = document.getElementById("btnEnviar");
-let idTimeout;
 
 btnenviar.addEventListener("click", function(event){
     event.preventDefault();
-    let email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
-    let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
-    let alertError = document.getElementById("alertError");
-    let flexCheckDefault = document.getElementById("flexCheckDefault");
-    let exampleFormControlInput2 = document.getElementById("exampleFormControlInput2")
-    exampleFormControlTextarea1.value = exampleFormControlTextarea1.value.trim();
-    alertError.style.display="none";
-    alertError.innerHTML="";
-    validos=0;
+  let inputNombre = document.getElementById('nombre');
+  let inputMail = document.getElementById('correo');
+  let inputTel = document.getElementById('telefono');
+  let inputMensaje = document.getElementById('especificaciones');
+  let alertError = document.getElementById("alertError");
+  let inputImagen = document.getElementById("imagen");
+  let email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (exampleFormControlTextarea1.value.trim().replaceAll("  ","").length < 20){
+  
+
+  
+  
+  inputMensaje.value = inputMensaje.value.trim();
+  alertError.style.display = "none";
+  alertError.innerHTML = "";
+  validos = 0;
+
+    if (inputMensaje.value.trim().replaceAll("  ","").length < 20){
         alertError.innerHTML = "El mensaje debe contener 20 caracteres o mas";
         alertError.style.display="block";
-        exampleFormControlTextarea1.focus();
-        exampleFormControlTextarea1.select();
+        inputMensaje.focus();
+        inputMensaje.select();
     }else{
-        exampleFormControlTextarea1.style.border = "solid green 1px";
+        inputMensaje.style.border = "solid green 1px";
         validos++;}
+    if (inputNombre.value.trim().replaceAll("  ","").length < 3){
+          alertError.innerHTML = "El nombre debe contener 3 caracteres o mas";
+          alertError.style.display="block";
+          inputNombre.focus();
+          inputNombre.select();
+    }else{
+          inputNombre.style.border = "solid green 1px";
+          validos++;}
+      
 
-    if (exampleFormControlInput1.value.match(email) == null){
+    if (inputMail.value.match(email) == null){
         alertError.style.display="block";
         alertError.innerHTML += "<br/>El correo electronico no es valido.";
     }else{
-        exampleFormControlInput1.style.border = "solid green 1px";
+        inputMail.style.border = "solid green 1px";
         validos++;}
-    let RFCRegex = /^[A-Z]{4}[0-9]{6}[A-Z0-9]{3}$/;
-    if (exampleFormControlInput2.value.match(RFCRegex) == null){
+    let telefonorex = /^\+52 \d{2} \d{4} \d{4}$/;
+    if (inputTel.value.match(telefonorex) == null){
         alertError.style.display="block";
-        alertError.innerHTML += "<br/>El RFC no es valido.";
+        alertError.innerHTML += "<br/>El formato de telefono no es valido ejemplo: +52 65 6192 0273";
     }else{
-        exampleFormControlInput2.style.border = "solid green 1px";
+        inputTel.style.border = "solid green 1px";
         validos++;
     }
     if ((idTimeout != undefined) && (idTimeout!=null)){
         clearTimeout(idTimeout);
     }
+ 
+    // Allowing file type
+    let regex = new RegExp(/[^\s]+(.*?).(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$/);
+    if (inputImagen.value.match(regex) == null) {
+      alertError.style.display="block";
+      alertError.innerHTML += "<br/> El formato de imagen es invalido solo agregar archivos .jpg o .png"
+    }else{
+      inputImagen.style.border = "solid green 1px";
+      validos++;
+    }
     //if Ternario
-    alertError.innerHTML += (!flexCheckDefault.checked)?"<br/>Debes aceptar los terminos y condiciones":"";
+    //alertError.innerHTML += (!flexCheckDefault.checked)?"<br/>Debes aceptar los terminos y condiciones":"";
 
     /*if (!flexCheckDefault.checked){
         alertError.innerHTML += "<br/>Debes aceptar los terminos y condiciones";
     }*/
-    if (validos == 3){
+    if (validos == 5){
         setTimeout(function(){
-            exampleFormControlInput1.style.border ="";
-            exampleFormControlInput2.style.border="";
-            exampleFormControlTextarea1.style.border="";
+            inputMail.style.border ="";
+            inputMensaje.style.border="";
+            inputTel.style.border="";
+            inputNombre.style.border="";
         }, 3000);
     }
 });//JC validaciones
