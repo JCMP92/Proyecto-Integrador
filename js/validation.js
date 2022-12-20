eaf71ede8a6eaa6ab75319ded9963b760a78669e
Mildred <mildredmrtnz1@gmail.com>
2022-12-19 18:42:38 -0600

    Comprobación de las validaciones de los formularios

--- js/validation.js
+++ js/validation.js
@@ -1,3 +1,5 @@
+
+
 let btnenviar = document.getElementById("btnEnviar");
 let idTimeout;
 
@@ -20,6 +22,7 @@ btnenviar.addEventListener("click", function(event){
         alertError.style.display="block";
         inputMensaje.focus();
         inputMensaje.select();
+        inputMensaje.style.border = "solid red 1px";
     }else{
         inputMensaje.style.border = "solid green 1px";
         validos++;}
@@ -28,6 +31,7 @@ btnenviar.addEventListener("click", function(event){
           alertError.style.display="block";
           inputNombre.focus();
           inputNombre.select();
+          inputNombre.style.border = "solid red 1px";
     }else{
           inputNombre.style.border = "solid green 1px";
           validos++;}
@@ -36,6 +40,7 @@ btnenviar.addEventListener("click", function(event){
     if (inputMail.value.match(email) == null){
         alertError.style.display="block";
         alertError.innerHTML += "<br/>El correo electronico no es valido.";
+        inputMail.style.border = "solid red 1px";
     }else{
         inputMail.style.border = "solid green 1px";
         validos++;}
@@ -43,17 +48,20 @@ btnenviar.addEventListener("click", function(event){
     if (inputTel.value.match(telefonorex) == null){
         alertError.style.display="block";
         alertError.innerHTML += "<br/>El formato de telefono no es valido ejemplo: +52 65 6192 0273";
+        inputTel.style.border = "solid red 1px";
     }else{
         inputTel.style.border = "solid green 1px";
         validos++;
     }
     if ((idTimeout != undefined) && (idTimeout!=null)){
         clearTimeout(idTimeout);
+      
     }
     let regex = new RegExp(/[^\s]+(.*?).(jpg|jpeg|png|JPG|JPEG|PNG)$/);
     if (inputImagen.value.match(regex) == null) {
       alertError.style.display="block";
       alertError.innerHTML += "<br/> El formato de imagen es invalido solo agregar archivos .jpg o .png"
+      inputImagen.style.border = "solid red 1px";
     }else{
       inputImagen.style.border = "solid green 1px";
       validos++;
@@ -64,7 +72,7 @@ btnenviar.addEventListener("click", function(event){
     /*if (!flexCheckDefault.checked){
         alertError.innerHTML += "<br/>Debes aceptar los terminos y condiciones";
     }*/
-    if (validos == 5){
+    if (validos == 4){
         setTimeout(function(){
             inputMail.style.border ="";
             inputMensaje.style.border="";
