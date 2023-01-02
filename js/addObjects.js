0f3e5be45b13af43a718a22bc45a9157d28b9de5
Juan C. Montes Pesquera <arq_juanmp@outlook.com>
2023-01-02 11:35:13 -0600

    Modificaicón de html tienda, para mostrar lista de objetos

--- js/addObjects.js
+++ js/addObjects.js
@@ -1,110 +1,113 @@
 //Objetos Libreta
 const libreta01 = {
-    // Nombre de la libreta
-    name: "Libreta Gato Alicia",
-    // Precio de la libreta
-    price: 200,
-    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
-    description: "Libreta cosida, tamaño media carta, 100 hojas rayadas", 
-    // Ubicación de la imagen de la libreta
-    image: "libretasImg/gatoalicia.jpg",
-}
+  // Nombre de la libreta
+  name: 'Libreta Gato Alicia',
+  // Precio de la libreta
+  price: 200,
+  // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+  description: 'Libreta cosida, tamaño media carta, 100 hojas rayadas',
+  // Ubicación de la imagen de la libreta
+  image: 'Images/libretasImg/gatoalicia.jpg',
+};
 
 const libreta02 = {
-    // Nombre de la libreta
-    name: "Libreta Girasoles",
-    // Precio de la libreta
-    price: 200,
-    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
-    description: "Libreta de arillo, tamaño media carta, 100 hojas rayadas", 
-    // Ubicación de la imagen de la libreta
-    image: "libretasImg/girasoles.jpg",
-}
+  // Nombre de la libreta
+  name: 'Libreta Girasoles',
+  // Precio de la libreta
+  price: 200,
+  // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+  description: 'Libreta de arillo, tamaño media carta, 100 hojas rayadas',
+  // Ubicación de la imagen de la libreta
+  image: 'libretasImg/girasoles.jpg',
+};
 
 const libreta03 = {
-    // Nombre de la libreta
-    name: "Libreta Gravity Falls",
-    // Precio de la libreta
-    price: 200,
-    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
-    description: "Libreta cosida, tamaño media carta, 100 hojas rayadas", 
-    // Ubicación de la imagen de la libreta
-    image: "libretasImg/gravity.jpg",
-}
+  // Nombre de la libreta
+  name: 'Libreta Gravity Falls',
+  // Precio de la libreta
+  price: 200,
+  // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+  description: 'Libreta cosida, tamaño media carta, 100 hojas rayadas',
+  // Ubicación de la imagen de la libreta
+  image: 'libretasImg/gravity.jpg',
+};
 
 const libreta04 = {
-    // Nombre de la libreta
-    name: "Libreta Pink Floyd",
-    // Precio de la libreta
-    price: 200,
-    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
-    description: "Libreta de arillo, tamaño media carta, 100 hojas rayadas",  
-    // Ubicación de la imagen de la libreta
-    image: "libretasImg/pinkfloyd.jpg",
-}
+  // Nombre de la libreta
+  name: 'Libreta Pink Floyd',
+  // Precio de la libreta
+  price: 200,
+  // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+  description: 'Libreta de arillo, tamaño media carta, 100 hojas rayadas',
+  // Ubicación de la imagen de la libreta
+  image: 'libretasImg/pinkfloyd.jpg',
+};
 
 const libreta05 = {
-    // Nombre de la libreta
-    name: "Libreta El principito",
-    // Precio de la libreta
-    price: 200,
-    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
-    description: "Libreta cosida, tamaño media carta, 100 hojas rayadas", 
-    // Ubicación de la imagen de la libreta
-    image: "libretasImg/principito.jpg",
-}
+  // Nombre de la libreta
+  name: 'Libreta El principito',
+  // Precio de la libreta
+  price: 200,
+  // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+  description: 'Libreta cosida, tamaño media carta, 100 hojas rayadas',
+  // Ubicación de la imagen de la libreta
+  image: 'libretasImg/principito.jpg',
+};
 
 const amigu01 = {
-    // Nombre del amigurumi
-    name: "Aguacates",
-    // Precio del amigurumi
-    price: 300,
-    // Descripción 
-    description: "Dos aguacates tejidos de 15 cm de altura", 
-    // Ubicación de la imagen 
-    image: "images/amiguImg/aguacates.jpg",
-}
+  // Nombre del amigurumi
+  name: 'Aguacates',
+  // Precio del amigurumi
+  price: 300,
+  // Descripción
+  description: 'Dos aguacates tejidos de 15 cm de altura',
+  // Ubicación de la imagen
+  image: 'Images/amiguImg/aguacates.jpg',
+};
 
 const amigu02 = {
-    // Nombre del amigurumi
-    name: "Conejo Evan",
-    // Precio del amigurumi
-    price: 200,
-    // Descripción 
-    description: "Conejo tejido de 15 cm de altura", 
-    // Ubicación de la imagen 
-    image: "images/amiguImg/conejoEvan.jpg",
-}
+  // Nombre del amigurumi
+  name: 'Conejo Evan',
+  // Precio del amigurumi
+  price: 200,
+  // Descripción
+  description: 'Conejo tejido de 15 cm de altura',
+  // Ubicación de la imagen
+  image: 'images/amiguImg/conejoEvan.jpg',
+};
 
 const amigu03 = {
-    // Nombre del amigurumi
-    name: "Monkey",
-    // Precio del amigurumi
-    price: 200,
-    // Descripción 
-    description: "Mono tejido de 15 cm de altura", 
-    // Ubicación de la imagen 
-    image: "images/amiguImg/monkey.jpg",
-}
+  // Nombre del amigurumi
+  name: 'Monkey',
+  // Precio del amigurumi
+  price: 200,
+  // Descripción
+  description: 'Mono tejido de 15 cm de altura',
+  // Ubicación de la imagen
+  image: 'images/amiguImg/monkey.jpg',
+};
 
 const amigu04 = {
-    // Nombre del amigurumi
-    name: "Monster Inc",
-    // Precio del amigurumi
-    price: 250,
-    // Descripción 
-    description: "figuras tejidas de Mike y Sully de Monsters Inc, 15 cm de altura", 
-    // Ubicación de la imagen 
-    image: "images/amiguImg/monster.jpg",
-}
+  // Nombre del amigurumi
+  name: 'Monster Inc',
+  // Precio del amigurumi
+  price: 250,
+  // Descripción
+  description:
+    'figuras tejidas de Mike y Sully de Monsters Inc, 15 cm de altura',
+  // Ubicación de la imagen
+  image: 'images/amiguImg/monster.jpg',
+};
 
 const amigu05 = {
-    // Nombre del amigurumi
-    name: "Spiderman",
-    // Precio del amigurumi
-    price: 200,
-    // Descripción 
-    description: "Spiderman tejido de 15 cm de altura", 
-    // Ubicación de la imagen 
-    image: "images/amiguImg/spiderman.jpg",
-}
+  // Nombre del amigurumi
+  name: 'Spiderman',
+  // Precio del amigurumi
+  price: 200,
+  // Descripción
+  description: 'Spiderman tejido de 15 cm de altura',
+  // Ubicación de la imagen
+  image: 'images/amiguImg/spiderman.jpg',
+};
+
+console.log(amigu01); //Para comprobar que ya esté linkeada la pág
