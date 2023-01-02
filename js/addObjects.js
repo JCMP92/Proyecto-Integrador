0952377e1d1e2dbf68bff954478446eee4f45a5f
verum <rodriguez.ibarra.juan.carlos95@gmail.com>
2023-01-02 11:02:45 -0600

    se creó el archivo js addObjects y se crearon los objetos de libretas

---
+++ js/addObjects.js
@@ -0,0 +1,55 @@
+//Objetos Libreta
+const libreta01 = {
+    // Nombre de la libreta
+    name: "Libreta Gato Alicia",
+    // Precio de la libreta
+    price: 200,
+    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+    description: "Libreta cosida, tamaño media carta, 100 hojas rayadas", 
+    // Ubicación de la imagen de la libreta
+    image: "libretasImg/gatoalicia.jpg",
+}
+
+const libreta02 = {
+    // Nombre de la libreta
+    name: "Libreta Girasoles",
+    // Precio de la libreta
+    price: 200,
+    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+    description: "Libreta de arillo, tamaño media carta, 100 hojas rayadas", 
+    // Ubicación de la imagen de la libreta
+    image: "libretasImg/girasoles.jpg",
+}
+
+const libreta03 = {
+    // Nombre de la libreta
+    name: "Libreta Gravity Falls",
+    // Precio de la libreta
+    price: 200,
+    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+    description: "Libreta cosida, tamaño media carta, 100 hojas rayadas", 
+    // Ubicación de la imagen de la libreta
+    image: "libretasImg/gravity.jpg",
+}
+
+const libreta04 = {
+    // Nombre de la libreta
+    name: "Libreta Pink Floyd",
+    // Precio de la libreta
+    price: 200,
+    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+    description: "Libreta de arillo, tamaño media carta, 100 hojas rayadas",  
+    // Ubicación de la imagen de la libreta
+    image: "libretasImg/pinkfloyd.jpg",
+}
+
+const libreta05 = {
+    // Nombre de la libreta
+    name: "Libreta El principito",
+    // Precio de la libreta
+    price: 200,
+    // Descripción de la libreta (tamaño, tipo de hoja, número de hojas, tipo de encuadernación)
+    description: "Libreta cosida, tamaño media carta, 100 hojas rayadas", 
+    // Ubicación de la imagen de la libreta
+    image: "libretasImg/principito.jpg",
+}
