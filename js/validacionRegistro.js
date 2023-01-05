fa2f72d06c36f4e4faa5c1f3e18999a9f55abd3a
Juan C. Montes Pesquera <arq_juanmp@outlook.com>
2023-01-04 20:32:12 -0600

    Validaciones a nombre, precio y descripción modificadas

--- js/validacionRegistro.js
+++ js/validacionRegistro.js
@@ -1,9 +1,49 @@
 let btnenviar = document.getElementById('btnEnviar');
-let inputImagen = document.getElementById('imagen');
-let inputNombre = document.getElementById('nombre');
+let idTimeout;
+let validos = 0;
+const priceRegex = /^\$\d+(\.\d{2})?$/;
 
-//Nombre
-if (inputNombre.value.trim().replaceAll('  ', '').length < 3) {
+
+
+// //imagen
+// function isValidImage(filename) {
+//   const allowedExtensions = ['jpg', 'jpeg', 'png'];
+//   return allowedExtensions.some((ext) => filename.endsWith(ext));
+// }
+
+// if (isValidImage('my-image.jpg')) {
+//   // es una imagen válida
+// } else {
+//   // no es una imagen válida
+// } //if
+
+// function isValidImage(filename) {
+//   const allowedExtensions = ['jpg', 'jpeg', 'png'];
+//   const [, extension] = filename.split('.');
+//   return allowedExtensions.includes(extension);
+// } //if
+
+// //Descripción
+// if (string.length >= 20) {
+//   // La cadena tiene al menos 20 caracteres
+// } else {
+//   // La cadena tiene menos de 20 caracteres
+// }
+
+btnenviar.addEventListener('click', function (event) {
+  event.preventDefault();
+  let inputNombre = document.getElementById('name');
+  let inputPrice = document.getElementById('price');
+  let inputDescripcion = document.getElementById('description');
+  let inputImg = document.getElementById('imgproduct');
+  let alertError = document.getElementById('alertError');
+
+  alertError.style.display = 'none';
+  alertError.innerHTML = '';
+  validos = 0;
+
+  //Nombre
+  if (inputNombre.value.trim().replaceAll('  ', '').length < 3) {
     alertError.innerHTML += 'El nombre debe contener 3 caracteres o más.';
     alertError.style.display = 'block';
     inputNombre.focus();
@@ -14,40 +54,39 @@ if (inputNombre.value.trim().replaceAll('  ', '').length < 3) {
     validos++;
   }
 
-
-
-//Precio
-  function validatePrice(price) {
-    const regex = /^\$\d+(\.\d{2})?$/;
-    return regex.test(price);
-  }
-  
-  console.log(validatePrice('$12.34')); // true
-  console.log(validatePrice('$12')); // true
-  console.log(validatePrice('$12.345')); // false
-  console.log(validatePrice('12.34')); // false
-
-//imagen
-  function isValidImage(filename) {
-    const allowedExtensions = ['jpg', 'jpeg', 'png'];
-    return allowedExtensions.some(ext => filename.endsWith(ext));
-  }
-
-  if (isValidImage('my-image.jpg')) {
-    // es una imagen válida
+  //Precio
+  if (inputPrice.value.match(priceRegex) == null) {
+    alertError.style.display = 'block';
+    alertError.innerHTML += '<br/>El formato de precio no es válido.';
+    inputPrice.style.border = 'solid red 1px';
   } else {
-    // no es una imagen válida
-  }//if
-
-  function isValidImage(filename) {
-    const allowedExtensions = ['jpg', 'jpeg', 'png'];
-    const [, extension] = filename.split('.');
-    return allowedExtensions.includes(extension);
-  }//if
-
-  //Descripción 
-  if (string.length >= 20) {
-    // La cadena tiene al menos 20 caracteres
-  } else {
-    // La cadena tiene menos de 20 caracteres
+    inputMail.style.border = 'solid green 1px';
+    validos++;
   }
+
+  //Descripción
+  if (inputDescripcion.value.trim().replaceAll('  ', '').length < 20) {
+    alertError.innerHTML +=
+      '<br/>El mensaje debe contener 20 caracteres o más.';
+    alertError.style.display = 'block';
+    inputDescripcion.focus();
+    inputDescripcion.select();
+    inputDescripcion.style.border = 'solid red 1px';
+  } else {
+    inputDescripcion.style.border = 'solid green 1px';
+    validos++;
+  }
+
+  if (idTimeout != undefined && idTimeout != null) {
+    clearTimeout(idTimeout);
+  }
+  if (validos == 4) {
+    setTimeout(function () {
+      inputMail.style.border = '';
+      inputMensaje.style.border = '';
+      inputTel.style.border = '';
+      inputNombre.style.border = '';
+    }, 3000);
+    console.log('ready');
+  }
+});
