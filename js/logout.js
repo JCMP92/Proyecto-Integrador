fa84078f32deef9434c008403e78c471148d721b
Juan C. Montes Pesquera <arq_juanmp@outlook.com>
2023-01-16 18:41:17 -0600

    Modifique nav bar para mostrar botones de log in o log out. Añadí event listeners a botones

--- js/logout.js
+++ js/logout.js
@@ -1,6 +1,34 @@
-function logout() {
-    sessionStorage.removeItem("currentUser");
-    exampleModal.style.display;
-    window.location.href = "../login.html";
+let logoutbtn = document.getElementById('btnLogout');
+let logoutbtnMob = document.getElementById('btnLogoutMob');
+let loginbtn = document.getElementById('bntLogin');
+let loginbtnMob = document.getElementById('bntLoginMob');
 
+//Boolean login
+let isLogged = sessionStorage.getItem('currentUser');
+console.log(isLogged);
+let loged = isLogged ? true : false;
+console.log(loged);
+
+if (!isLogged) {
+  logoutbtn.style.display = 'none';
+  logoutbtnMob.style.display = 'none';
+} else if (isLogged) {
+  loginbtn.style.display = 'none';
+  loginbtnMob.style.display = 'none';
 }
+
+function logout() {
+  sessionStorage.removeItem('currentUser');
+  exampleModal.style.display;
+  window.location.href = '../index.html';
+}
+
+logoutbtn.addEventListener('click', function (e) {
+  e.preventDefault;
+  logout();
+});
+
+logoutbtnMob.addEventListener('click', function (e) {
+  e.preventDefault;
+  logout();
+});
--- js/validacionLogin.js
+++ js/validacionLogin.js
@@ -2,13 +2,12 @@ let btnenviar = document.getElementById('btnEnviar');
 let idTimeout;
 let datosUsuario = [];
 
-
 btnenviar.addEventListener('click', function (event) {
   event.preventDefault();
   let inputUsuario = document.getElementById('usuario');
   let inputPassword = document.getElementById('password');
   let alertError = document.getElementById('alertError');
- 
+
   let pass1 = document.getElementById('password');
   let email =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -17,48 +16,51 @@ btnenviar.addEventListener('click', function (event) {
   alertError.innerHTML = '';
   validos = 0;
 
-  if (inputUsuario.value.match(email) == null && pass1.value.length == 0 ) {
+  if (inputUsuario.value.match(email) == null && pass1.value.length == 0) {
     alertError.style.display = 'block';
     alertError.innerHTML += '<br/>Datos inválidos.';
     inputUsuario.style.border = 'solid red 1px';
     inputPassword.style.border = 'solid red 1px';
   } else {
     inputUsuario.style.border = 'solid green 1px';
-    inputPassword.style.border = 'solid green 1px';  
-  //Modal
+    inputPassword.style.border = 'solid green 1px';
+    //Modal
     validos++;
   }
-  
 
-  function login(username,password) {
-    let users = JSON.parse(localStorage.getItem("datosUsuario"));
+  function login(username, password) {
+    let users = JSON.parse(localStorage.getItem('datosUsuario'));
     for (let i = 0; i < users.length; i++) {
-    if (users[i].correo == username && users[i].password == password) {
-    sessionStorage.setItem("currentUser", username);
-    window.location.href = "../index.html";
-    return;
-    }
+      if (users[i].correo == username && users[i].password == password) {
+        sessionStorage.setItem('currentUser', username);
+        window.location.href = '../index.html';
+        return;
+      }
     }
     alertError.innerHTML += '<br/>Usuario o contraseña incorrectos.';
-    }
-  btnenviar.addEventListener('click', function (event) {
-    event.preventDefault;
-    let tmp = localStorage.getItem('datosUsuario');
-    if (tmp != null) {
-      datosUsuario = JSON.parse(tmp);
-      let inputUsuario = document.getElementById('usuario');
-  let inputPassword = document.getElementById('password');
+  }
 
-  login(inputUsuario.value,inputPassword.value);
-   
-    // console.log(datosUsuario[0].correo);
-    // console.log(datosUsuario[0].password);
-    
+  let tmp = localStorage.getItem('datosUsuario');
+  if (tmp != null) {
+    datosUsuario = JSON.parse(tmp);
+    let inputUsuario = document.getElementById('usuario');
+    let inputPassword = document.getElementById('password');
 
+    login(inputUsuario.value, inputPassword.value);
+  }
+});
 
+// btnenviar.addEventListener('click', function (event) {
+//   event.preventDefault;
+//   let tmp = localStorage.getItem('datosUsuario');
+//   if (tmp != null) {
+//     datosUsuario = JSON.parse(tmp);
+//     let inputUsuario = document.getElementById('usuario');
+//     let inputPassword = document.getElementById('password');
 
-      
-    
-    }
-  })
-})
+//     login(inputUsuario.value, inputPassword.value);
+
+//     // console.log(datosUsuario[0].correo);
+//     // console.log(datosUsuario[0].password);
+//   }
+// });
